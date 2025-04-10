594c54f5c17b86b26767a6c5428454e0734b683e
lindoro7 <lindoro2012@yandex.ru>
2025-04-10 14:00:39 +0300

    Add basic styles with MUI, theme toggler, mocked users, construction of app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,189 @@
 import React from "react";
+import {
+  createTheme,
+  ThemeProvider,
+  Typography,
+  TextField,
+  Box,
+  Pagination,
+  Card,
+  CardContent,
+  CardMedia,
+  Button,
+  IconButton,
+} from "@mui/material";
+import { Brightness4, Brightness7 } from "@mui/icons-material";
+import users from "./users";
 
-import { createTheme, ThemeProvider, Typography } from "@mui/material";
+const App = () => {
+  const [page, setPage] = React.useState(1);
+  const [darkMode, setDarkMode] = React.useState(false);
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#1976d2",
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#1976d2",
+      },
+      secondary: {
+        main: "#dc004e",
+      },
     },
-    secondary: {
-      main: "#dc004e",
-    },
-  },
-});
+  });
 
-function App() {
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <Typography variant='h1' component='h2'>
-          Привет, мир!
-        </Typography>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          padding: "20px",
+          boxSizing: "border-box",
+          backgroundColor: theme.palette.background.default,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant='h4'
+            component='h2'
+            align='center'
+            gutterBottom
+            color='text.primary'
+          >
+            Поиск пользователей GITHUB
+          </Typography>
+          <IconButton onClick={toggleTheme} color='inherit'>
+            {darkMode ? (
+              <Brightness7 sx={{ color: "yellow" }} />
+            ) : (
+              <Brightness4 sx={{ color: theme.palette.text.primary }} />
+            )}
+          </IconButton>
+        </div>
+
+        <TextField
+          id='search-input'
+          variant='outlined'
+          label='Поиск'
+          fullWidth
+          margin='normal'
+          slotProps={{
+            InputLabel: {
+              sx: { color: theme.palette.text.primary },
+            },
+            outlinedInput: {
+              sx: {
+                "& fieldset": {
+                  borderColor: theme.palette.text.primary,
+                },
+                "&:hover fieldset": {
+                  borderColor: theme.palette.primary.main,
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: theme.palette.primary.main,
+                },
+              },
+            },
+          }}
+        />
+
+        <Box
+          sx={{
+            marginTop: "20px",
+            padding: "10px",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+            flexGrow: "1",
+            display: "grid",
+            gridTemplateColumns: {
+              xs: "repeat(2,1fr)",
+              sm: "repeat(4,1fr)",
+              md: "repeat(7,1fr)",
+            },
+            gridTemplateRows: "repeat(3, 1fr)",
+            gap: "10px",
+            minHeight: "300px",
+            overflowY: "auto",
+          }}
+        >
+          {users.map((user) => (
+            <Card
+              key={user.login}
+              sx={{ display: "flex", flexDirection: "column", height: "100%" }}
+            >
+              <CardContent
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  flexGrow: "1",
+                }}
+              >
+                <CardMedia
+                  component='img'
+                  sx={{
+                    height: "100px",
+                    width: "100px",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                  }}
+                  image={user.avatar_url}
+                  alt={user.login}
+                />
+                <Typography
+                  variant='body2'
+                  component='div'
+                  sx={{ marginTop: "1" }}
+                >
+                  {user.login}
+                </Typography>
+              </CardContent>
+              <Button
+                size='small'
+                variant='outlined'
+                color='primary'
+                href={user.html_url}
+                target='_blank'
+                sx={{
+                  marginTop: "auto",
+                  marginBottom: "10px",
+                  alignSelf: "center",
+                }}
+              >
+                Профиль
+              </Button>
+            </Card>
+          ))}
+        </Box>
+
+        <Box
+          sx={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
+        >
+          <Pagination
+            count={Math.ceil(users.length / (7 * 3))}
+            page={page}
+            onChange={handleChange}
+            color='primary'
+          />
+        </Box>
       </div>
     </ThemeProvider>
   );
-}
+};
+
 export default App;
---
+++ src/users.js
@@ -0,0 +1,105 @@
+// users.js
+const users = [
+  {
+    login: "octocat",
+    avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
+    html_url: "https://github.com/octocat",
+  },
+  {
+    login: "defunkt",
+    avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
+    html_url: "https://github.com/defunkt",
+  },
+  {
+    login: "mojombo",
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo",
+  },
+  {
+    login: "pjhyett",
+    avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
+    html_url: "https://github.com/pjhyett",
+  },
+  {
+    login: "wycats",
+    avatar_url: "https://avatars.githubusercontent.com/u/4?v=4",
+    html_url: "https://github.com/wycats",
+  },
+  {
+    login: "ezrazhang",
+    avatar_url: "https://avatars.githubusercontent.com/u/5?v=4",
+    html_url: "https://github.com/ezrazhang",
+  },
+  {
+    login: "ivey",
+    avatar_url: "https://avatars.githubusercontent.com/u/6?v=4",
+    html_url: "https://github.com/ivey",
+  },
+  {
+    login: "vanpelt",
+    avatar_url: "https://avatars.githubusercontent.com/u/7?v=4",
+    html_url: "https://github.com/vanpelt",
+  },
+  {
+    login: "tenderlove",
+    avatar_url: "https://avatars.githubusercontent.com/u/8?v=4",
+    html_url: "https://github.com/tenderlove",
+  },
+  {
+    login: "kristof",
+    avatar_url: "https://avatars.githubusercontent.com/u/9?v=4",
+    html_url: "https://github.com/kristof",
+  },
+  {
+    login: "sferik",
+    avatar_url: "https://avatars.githubusercontent.com/u/10?v=4",
+    html_url: "https://github.com/sferik",
+  },
+  {
+    login: "jasonm23",
+    avatar_url: " https://avatars.githubusercontent.com/u/11?v=4",
+    html_url: " https://github.com/jasonm23",
+  },
+  {
+    login: "benhoskings",
+    avatar_url: " https://avatars.githubusercontent.com/u/12?v=4",
+    html_url: " https://github.com/benhoskings",
+  },
+  {
+    login: "tchapi",
+    avatar_url: " https://avatars.githubusercontent.com/u/13?v=4",
+    html_url: " https://github.com/tchapi",
+  },
+  {
+    login: "bkeepers",
+    avatar_url: " https://avatars.githubusercontent.com/u/14?v=4",
+    html_url: " https://github.com/bkeepers",
+  },
+  {
+    login: "marcelotduarte",
+    avatar_url: " https://avatars.githubusercontent.com/u/15?v=4",
+    html_url: " https://github.com/marcelotduarte",
+  },
+  {
+    login: "sindresorhus",
+    avatar_url: " https://avatars.githubusercontent.com/u/16?v=4",
+    html_url: " https://github.com/sindresorhus",
+  },
+  {
+    login: "addyosmani",
+    avatar_url: " https://avatars.githubusercontent.com/u/17?v=4",
+    html_url: " https://github.com/addyosmani",
+  },
+  {
+    login: "gaearon",
+    avatar_url: " https://avatars.githubusercontent.com/u/18?v=4",
+    html_url: " https://github.com/gaearon",
+  },
+  {
+    login: "kentcdodds",
+    avatar_url: " https://avatars.githubusercontent.com/u/19?v=4",
+    html_url: " https://github.com/kentcdodds",
+  },
+];
+
+export default users;
