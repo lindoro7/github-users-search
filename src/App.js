a79738a95014822915d658b010cf899a12e5d47b
lindoro7 <lindoro2012@yandex.ru>
2025-04-10 11:38:19 +0300

    Add theme to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { Typography } from "@mui/material";
+
+import { createTheme, ThemeProvider, Typography } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
 
 function App() {
   return (
-    <div>
-      <Typography variant='h1' component='h2'>
-        Привет, мир!
-      </Typography>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Typography variant='h1' component='h2'>
+          Привет, мир!
+        </Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 export default App;
